22eda77a2df1d73fee70c2da8ee459cd2ca697c3
Nathane77 <hamidoulila14@gmail.com>
2025-03-27 12:49:25 +0100

    removed console logs

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,14 @@
 const btns = document.querySelectorAll('.gametype')
-
 const root = document.querySelector(':root')
 
 btns.forEach(btn =>{
+    
     btn.addEventListener('mouseover', function(){
         root.style.setProperty("--grad1","#0065a2")
         root.style.setProperty("--grad2", "#004c88")
         root.style.setProperty("--grad3", "#00336d")
         root.style.setProperty("--grad4", "#001d52")
         root.style.setProperty("--grad5", "#030038")  
-        console.log("yes")
     })
 
     btn.addEventListener('mouseout', function(){
@@ -18,7 +17,6 @@ btns.forEach(btn =>{
         root.style.setProperty("--grad3", "#0065a2")
         root.style.setProperty("--grad4", "#00346d")
         root.style.setProperty("--grad5", "#030038")  
-        console.log("no")
     })
 
     btn.addEventListener('click', function(){
